76b5285aa2c71fea5e4a1e4af8cbf695d0b28115
Vadimcha <BelovBadim2014@yandex.ru>
2023-04-23 18:13:12 +0300

    Made buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './style.sass'
 import Button from './Styled_components/Button/Button';
+import Link from './Styled_components/Link/Link';
 
 function App() {
     const links = ["Whereas", "Prices", "Our History", "About Us", "Contacts"]
   return (
     <div className="App">
         <div className="primeNav">
-            { links.map((link) => <Button content={link} />) }
+            { links.map((link) => <Button fz={"2em"} theme={"dark"} content={link} />) }
         </div>
-
+        <div className="primeNav" style={{ backgroundColor: "black" }}>
+            { links.map((link) => <Link fz={"2em"} href={"#"} theme={"dark"} content={link} />) }
+        </div>
     </div>
   );
 }
--- src/Styled_components/Button/Button.tsx
+++ src/Styled_components/Button/Button.tsx
@@ -9,8 +9,10 @@ interface buttonProps {
 }
 
 export default function Button({ content, onclick=()=>{}, theme="light", fz="0.9em" } : buttonProps) {
+    let bgColor = (theme == "light" ? "#e8e8e8" : "black")
+    let Color = (theme == "light" ? "black" : "#e8e8e8")
     return (
-        <button className="BUTTON">
+        <button onClick={onclick} className="BUTTON" style={{ fontSize: fz, color: Color, backgroundColor: bgColor }}>
             <p className="BLOCK">{content}</p>
             <p className="FIRST">{content}</p>
             <p className="SECOND">{content}</p>
